2ec3d80803098169b5bb5a85c6363aa071f4aec2
koppula.v <koppula.v@husky.neu.edu>
2019-11-23 19:46:16 -0500

    manage network upload

---
+++ Refugeeorg/src/Business/Network/Network.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Network;
+
+/**
+ *
+ * @author koppu
+ */
+public class Network {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+      
+    
+    
+
+}
